9c9d2b96a6b8f7674154ab7321c945e0274e2cd5
alexamoshina <alexamoshina@mail.ru>
2018-04-18 17:24:46 +0200

    envp

--- src/main/java/fr/univavignon/rodeo/api/EnvironmentProvider.java
+++ src/main/java/fr/univavignon/rodeo/api/EnvironmentProvider.java
@@ -1,6 +1,6 @@
 package fr.univavignon.rodeo.api;
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
 import java.util.List;
 
 public class EnvironmentProvider implements IEnvironmentProvider{
