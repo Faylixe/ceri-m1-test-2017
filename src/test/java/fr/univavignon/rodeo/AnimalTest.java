08183eae109c1f522a0e7e19fb9ae02525976176
Alexandra MOSHINA <alexamoshina@mail.ru>
2018-05-21 12:22:29 +0200

    test

--- src/test/java/fr/univavignon/rodeo/AnimalTest.java
+++ src/test/java/fr/univavignon/rodeo/AnimalTest.java
@@ -1,9 +1,20 @@
 package fr.univavignon.rodeo;
 
-import static org.junit.Assert.*;
-import org.mockito.Mockito;
+import fr.univavignon.rodeo.api.Animal;
+import org.junit.Assert;
+
 import org.junit.Test;
 
 public class AnimalTest extends IAnimalTest{
-	
-}
+
+	@Test
+	public void testAnimalOnCreate(){
+		final Animal animal = new Animal("Giraffe", 200, true, true, true);
+		Assert.assertNotNull(animal);
+		Assert.assertEquals(200, animal.getXP());
+		Assert.assertEquals("Giraffe", animal.getName());
+		Assert.assertTrue(animal.isBoss());
+		Assert.assertTrue(animal.isEndangered());
+		Assert.assertTrue(animal.isSecret());
+	}
+}   
